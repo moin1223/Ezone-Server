d73d75f28018ebcc744272961d3454e0068aaa9f
moin1223 <moinpuccse@gmail.com>
2022-07-05 21:57:47 +0600

    models done

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const app = express();
-app.listen(5000,()=>{
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config()
+const userRoute = require("./routes/user")
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>console.log("DB Connection Success"))
+.then((err)=>console.log(err))
+
+app.use(express.json())
+app.use("/api/users",userRoute)
+
+app.listen( process.env.PORT || 5000,()=>{
     console.log("Backen server is ready")
 })
---
+++ routes/user.js
@@ -0,0 +1,14 @@
+const router = require("express").Router()
+
+router.get("/test",(req,res)=>{
+res.send("user test succesfull!")
+})
+
+router.post("/userposttest",(req,res)=>{
+    const username = req.body.username;
+    console.log(username)
+})
+
+
+
+module.exports = router
