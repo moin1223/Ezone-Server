4bc12737444b36ab8580d75591043d1e7769da13
moin1223 <moinpuccse@gmail.com>
2022-07-18 23:53:28 +0600

    add jwt

--- index.js
+++ index.js
@@ -4,13 +4,18 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config()
 const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
 
 mongoose.connect(process.env.MONGO_URL)
 .then(()=>console.log("DB Connection Success"))
 .then((err)=>console.log(err))
 
+
+
 app.use(express.json())
 app.use("/api/users",userRoute)
+app.use("/api/auth",authRoute)
+
 
 app.listen( process.env.PORT || 5000,()=>{
     console.log("Backen server is ready")
--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,13 @@
 const router = require("express").Router()
 
-router.get("/test",(req,res)=>{
-res.send("user test succesfull!")
-})
+// router.get("/test",(req,res)=>{
+// res.send("user test succesfull!")
+// })
 
-router.post("/userposttest",(req,res)=>{
-    const username = req.body.username;
-    console.log(username)
-})
+// router.post("/userposttest",(req,res)=>{
+//     const username = req.body.username;
+//     console.log(username)
+// })
 
 
 
