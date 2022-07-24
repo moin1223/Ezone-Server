92326418d7f4c0d6ae08416f8ec889f5dff19c11
moin1223 <moinpuccse@gmail.com>
2022-07-24 16:33:08 +0600

    cart order product strip done

--- routes/auth.js
+++ routes/auth.js
@@ -26,6 +26,7 @@ router.post("/login", async (req, res) => {
     try {
         const user = await User.findOne({ username: req.body.username })
         !user && res.status(401).json("Wrong credentials!")
+        
         const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC);
         const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
 
